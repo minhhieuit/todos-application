54edf2934c7b76558498e89dee0e06c26c866872
Jacky Nguyen <giadinhcongnghe.com@gmail.com>
2022-01-05 08:09:23 +0700

    Them chuc nang fullscreen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
-var btnTest = document.getElementById('myBtn');
 var myModal = document.getElementById('modal');
 var btnClose = document.querySelector('#modal .close');
 
-btnClose.onclick = function (e) {
+btnClose.onclick = function () {
 	myModal.style.display = 'none';
 };
 
@@ -17,3 +16,26 @@ function closeModal() {
 function showListTodos() {
     myModal.style.display = 'block';
 }
+
+function go_full_screen(){
+    var elem = document.documentElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    }
+  }
+
+
+/**
+ * Các chức năng cơ bản
+ * 1 - Thêm mới công việc
+ * 2 - Sửa công việc
+ * 3 - Hoàn thành công việc
+ * Lưu trữ dữ liệu mỗi ngày trên firebase 
+ * Nghĩa là: cứ mỗi ngày sẽ tạo 1 document
+ */
